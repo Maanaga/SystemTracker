bb952c8bd16a40254d70ecf8229e9af98f82a93f
Luka Managadze <managaluka0@gmail.com>
2026-04-11 15:41:01 +0400

    Initial Commit

---
+++ SystemTracker/SystemTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemTrackerApp.swift
+//  SystemTracker
+//
+//  Created by Luka Managadze on 11/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct SystemTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
