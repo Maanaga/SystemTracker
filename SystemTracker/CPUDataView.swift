b7c9d059ec7e74a087fe76fecc843bfbe43ad411
Luka Managadze <managaluka0@gmail.com>
2026-04-11 15:53:50 +0400

    add: SystemKit package dependency

---
+++ SystemTracker/CPUDataView.swift
@@ -0,0 +1,14 @@
+//
+//  CPUDataView.swift
+//  SystemTracker
+//
+//  Created by Luka Managadze on 11/04/2026.
+//
+
+import SwiftUI
+
+struct CPUDataView: View {
+    var body: some View {
+        
+    }
+}
